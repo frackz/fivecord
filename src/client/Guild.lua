fbc0f588d112687c5eeb2cec6b72bf58abebe7d7
frackz <65610875+frackz@users.noreply.github.com>
2023-03-17 15:19:41 +0100

    asd

--- src/client/Guild.lua
+++ src/client/Guild.lua
@@ -6,14 +6,20 @@ function Guild:_init(data, client)
     self._api = client._api
 end
 
+--- Get guild id
+--- @return string | nil
 function Guild:getId()
     return self._data.raw.id
 end
 
+--- Get guild name
+--- @return string | nil
 function Guild:getName()
     return self._data.raw.name
 end
 
+--- Set guild name
+--- @return boolean
 function Guild:setName(name)
     local _, _, err = self._api:modifyGuild(self:getId(), {
         name = name
@@ -22,32 +28,55 @@ function Guild:setName(name)
     return err
 end
 
+--- Get guild by id
+--- @return table | boolean
 function Guild:getUser(id)
     local member = self._data.members[id]
     if member then
         return User(member, self._client)
     else
-        local status, data, err = self._client._api:getGuildMember(self:getId(), id)
+        local _, data, err = self._client._api:getGuildMember(self:getId(), id)
         if data and err then
             self._data.members[id] = data
             return User(data, self._client)
         else
-            return false, "invalid_user"
+            return false
         end
     end
 end
 
+--- Ban a user
+--- @param user table | string
+--- @param reason string | nil
+--- @param data table | nil
+function Guild:banUser(user, reason, data)
+
+end
+
+--- Kick a user
+--- @param user table | string
+--- @param reason string | nil
+function Guild:kickUser(user, reason)
+
+end
+
+--- Get channel
+--- @param id string
+--- @return table | boolean
 function Guild:getChannel(id)
     if not self._data.channels[id] then
-        return false, "invalid_channel"
+        return false
     end
 
     return Channel(self._data.channels[id], self._client, self:getId())
 end
 
+--- Get role
+--- @param id string
+--- @return table | boolean
 function Guild:getRole(id)
     if not self._data.roles[id] then
-        return false, "invalid_role"
+        return false
     end
 
     return Role(self._data.roles[id], self._client, self)
--- src/client/Message.lua
+++ src/client/Message.lua
@@ -8,46 +8,65 @@ function Message:_init(data, client)
     self._channel = data.channel_id
 end
 
--- Getters
+--- Get channel
+--- @return table | boolean
 function Message:getChannel()
     return self:getGuild():getChannel(self._channel, self._client, self._data.guild_id)
 end
 
+--- Get guild
+--- @return table | boolean
 function Message:getGuild()
     return self._client:getGuild(self._data.guild_id)
 end
 
+--- Get user
+--- @return table
 function Message:getUser()
     return User(self._data.author, self._client)
 end
 
+--- Get author / user
+--- @return table
 function Message:getAuthor()
     return self:getUser()
 end
 
+--- Get message-content
+--- @return string
 function Message:getContent()
     return self._data.content
 end
 
+--- Get message-embeds
+--- @return table | nil
 function Message:getEmbeds()
     return self._data.embeds
 end
 
+--- Get message-id
+--- @return string
 function Message:getId()
     return self._data.id
 end
 
--- Maintain
+--- Pin message
+--- @return boolean
 function Message:pin()
     local _, _, err = self._api:pinMessage(self._channel, self:getId())
     return err
 end
 
+--- Unpin message
+--- @return boolean
 function Message:unpin()
     local _, _, err =  self._api:unpinMessage(self._channel, self:getId())
     return err
 end
 
+--- Reply to message
+--- @param data string | table
+--- @return table | boolean
 function Message:reply(data)
     if type(data) == "string" then
         data = {content = data}
@@ -64,6 +83,9 @@ function Message:reply(data)
     return Message(data, self._client)
 end
 
+--- Edit message
+--- @param data string | table
+--- @return table | boolean
 function Message:edit(data)
     if type(data) == "string" then
         data = {content = data}
@@ -73,8 +95,11 @@ function Message:edit(data)
     return err
 end
 
+--- React to message
+--- @param data string | table
+--- @return boolean
 function Message:react(emoji)
-    local status, _, err = self._api:reactMessage(self._channel, self:getId(), emoji)
+    local _, _, err = self._api:reactMessage(self._channel, self:getId(), emoji)
     return err
 end
 
